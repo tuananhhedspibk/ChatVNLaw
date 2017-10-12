ebd427c28699e632b48cba4321c9e4b5e1db3de8
linhtm <tranlinh265@gmail.com>
2017-10-12 10:20:25 +0700

    add height and width attrs on image messages

--- src/lib/helper/message.js
+++ src/lib/helper/message.js
@@ -6,8 +6,13 @@ function exportItem(data,properties){
   item["sender_uid"] = data.val().sender_uid;
   item["msg_ts"] = data.val().msg_ts;
   item["contentType"] = data.val().contentType || '';
+  if(item["contenType"] && item["contenType"].includes("image")){
+    item["height"] = data.val().height;
+    item["width"] = data.val().width;
+  }
   item["name"] = data.val().name || '';
   item["downloadURL"] = data.val().downloadURL || '';
+  
   if(item["sender_uid"] === properties.uid){
     item["type"] = 0;
     item["image"] = data.val().photoURL;
