e59f02870ff636d55548871eaed72a7b0bb88148
linhtm <tranlinh265@gmail.com>
2017-10-24 14:18:22 +0700

    123

--- src/components/user/dashboard/main.js
+++ src/components/user/dashboard/main.js
@@ -12,42 +12,62 @@ import Breadcrumb from './components/breadcrumb';
 import {Container} from 'reactstrap';
 import Chat from './components/chat';
 import {EventEmitter} from 'fbemitter';
+import * as constant from '../../constants';
 
 import '../../../assets/styles/dashboard/style.css';
-// var emitter = new EventEmitter();
+var firebase = require('firebase');
+
 class UserDashBoard extends Component {  
   constructor(props){
     super(props);
+    this.state ={
+      currentUser: ''
+    }
     this.emitter = new EventEmitter();
   }
-
+  componentWillMount(){
+    var component = this;
+    firebase.auth().onAuthStateChanged(user =>{
+      if(user){
+        component.setState({currentUser: user})  
+      }else{
+        window.location = constant.BASE_URL + constant.SIGN_IN_URI;
+      }
+    })
+  }
   render() {
-    return(
-      <div className='app'>
-        <Header/>
-        <div className='app-body'>
-          <Sidebar {...this.props}/>
-          <main className='main'>
-            <Breadcrumb/>
-            <Container fluid>
-              <Switch>
-                <Route path="/lawyers/userinfo" name="UserInfo"
-                  render={(props) => (
-                    <UserInfo emitter={this.emitter} {...props} />)} />
-                <Route path="/lawyers/dashboard" name="Dashboard"
-                  component={DashBoard}/>
-                <Route path="/lawyers/notes" name="Notes"
-                  component={Note}/>
-                <Route path="/lawyers/calendar" name="Calendar"
-                  component={Calendar}/>
-                <Redirect from="/lawyers" to="/lawyers/dashboard"/>
-              </Switch>
-            </Container>
-          </main>
-          <Chat emitter={this.emitter}/>
+    if(this.state.currentUser){
+      return(
+        <div className='app'>
+          <Header/>
+          <div className='app-body'>
+            <Sidebar {...this.props}/>
+            <main className='main'>
+              <Breadcrumb/>
+              <Container fluid>
+                <Switch>
+                  <Route path="/lawyers/userinfo" name="UserInfo"
+                    render={(props) => (
+                      <UserInfo emitter={this.emitter} {...props} />)} />
+                  <Route path="/lawyers/dashboard" name="Dashboard"
+                    component={DashBoard}/>
+                  <Route path="/lawyers/notes" name="Notes"
+                    component={Note}/>
+                  <Route path="/lawyers/calendar" name="Calendar"
+                    component={Calendar}/>
+                  <Redirect from="/lawyers" to="/lawyers/dashboard"/>
+                </Switch>
+              </Container>
+            </main>
+            <Chat emitter={this.emitter}/>
+          </div>
         </div>
-      </div>
-    )
+      )
+    }else{
+      return (
+        <div></div>
+      )
+    }
   }
 }
 
