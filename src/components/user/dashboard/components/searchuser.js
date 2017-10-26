6e735739508efa787548a0c72029caeeb31323fd
linhtm <tranlinh265@gmail.com>
2017-10-26 10:41:21 +0700

    add search component

---
+++ src/components/user/dashboard/components/searchuser.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class SearchUser extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render(){
+        return (
+            <div >
+                <h1>search user</h1>
+            </div>
+        )
+    }
+}
+export default SearchUser;
