7c5250342f8dfd586eb3592f5cccbb8c730a7a0c
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2018-03-07 22:40:01 +0700

    Fix search law

--- src/components/searchlaw/result.js
+++ src/components/searchlaw/result.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import * as translate from 'counterpart';
 import * as constant from '../constants';
 
@@ -13,25 +14,49 @@ class Result extends Component {
 								href={constant.BASE_URL + constant.LAW_URI+ this.props.article.id}>
 									{this.props.article.title}
 							</a>
+							{
+								this.props.article.effect_status ==
+									translate('app.home.search_law.effect_status_1') ?
+								(
+									<label className='validated'>
+										{this.props.article.effect_status}
+									</label>
+								):
+								(
+									this.props.article.effect_status ==
+										translate('app.home.search_law.effect_status_2') ?
+									(
+										<label className='expired'>
+											{this.props.article.effect_status}
+										</label>
+									):
+									(
+										<label className='half-expired'>
+											{this.props.article.effect_status}
+										</label>
+									)
+								)
+							}
 						</div>
 						<div className='law-detail-content'>
 							<div className='law-status'>
 								<div className='public-day'>
 									<div className='sub-title'>
-										{translate('app.search.search_tool.order_by.order_by_1')}:
-										 {this.props.article.public_day}
+										<label>
+											{translate('app.search.search_tool.order_by.order_by_1')}:
+										</label>
+										{new Date(this.props.article.public_day).toLocaleDateString('en-US')}
 									</div>
 								</div>
 								<div className='effect-day'>
 									<div className='sub-title'>
-										{translate('app.search.search_tool.order_by.order_by_2')}:
-										 {this.props.article.effect_day}
+										<label>
+											{translate('app.search.search_tool.order_by.order_by_2')}:
+										</label>
+										{new Date(this.props.article.effect_day).toLocaleDateString('en-US')}
 									</div>
 								</div>
 								<div className='effect-status'>
-									<div className='value'>
-										{this.props.article.effect_status}
-									</div>
 								</div>
 							</div>
 						</div>
