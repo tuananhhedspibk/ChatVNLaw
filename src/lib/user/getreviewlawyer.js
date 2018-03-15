ba720ac7cb0c82ec5a3bb3c823ee3118982211c0
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2018-03-15 14:52:14 +0700

    Update for lawyer reviews

--- src/lib/user/getreviewlawyer.js
+++ src/lib/user/getreviewlawyer.js
@@ -1,21 +1,31 @@
 var firebase = require('firebase');
 
 function getReviewLawyer(properties){
-    let review = []
-    for(let i in properties.listReview){
-        firebase.database().ref(`users/${i}`).once('value', data => {
-            if(data.exists()){
-                review.push({
-                    img: data.val().photoURL,
-                    comment : properties.listReview[i].comment,
-                    star: properties.listReview[i].star
-                })
-                properties.component.setState({
-                    review: review
-                })
+    let reviews = [];
+    let ref = firebase.database().ref(`lawyers/${properties.lawyerId}/reviews`)
+        .orderByChild('created_at').endAt(properties.ts).limitToLast(properties.limit);
+    ref.once('value', data => {
+        if(data.exists()){
+            let reviews = properties.component.state.reviews;
+            let tmp = []
+
+            data.forEach(function(element) {
+                let item = {};
+                item['img'] = element.val().ava;
+                item['content'] = element.val().content;
+                item['star'] = element.val().star;
+                item['created_at'] = element.val().created_at;
+                tmp.push(item);
+            });
+            for(var idx = tmp.length - 1; idx >= 0; idx--) {
+                reviews.push(tmp[idx]);
             }
-        })
-    }
+
+            properties.component.setState({
+                reviews: reviews
+            });
+        }
+    })
 }
 
 module.exports = {
