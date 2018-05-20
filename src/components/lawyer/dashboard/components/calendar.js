8d5eed599f796337988e5048ab389fe1b28f36b4
tuananhhedspibk <tuananhhedspibk1@gmail.com>
2018-05-20 13:16:08 +0700

    Change calendar ui

--- src/components/lawyer/dashboard/components/calendar.js
+++ src/components/lawyer/dashboard/components/calendar.js
@@ -5,8 +5,8 @@ import $ from 'jquery';
 
 require('fullcalendar-reactwrapper/dist/css/fullcalendar.min.css');
 
-const CALENDAR_ID = '2qm6bc113of43opcqnsiernk5s@group.calendar.google.com';
-const API_KEY = 'AIzaSyCGFci7s06zoPhyF0d92Lu57BpGOkiszy0';
+const CALENDAR_ID = 'lkbc.team@gmail.com';
+const API_KEY = 'AIzaSyDX8G3uBcHdi3RP5AhBWjRL94GbJRpDEZE';
 let url = `https://www.googleapis.com/calendar/v3/calendars/${CALENDAR_ID}/events?key=${API_KEY}`;
 
 export function getEvents (callback) {
@@ -29,34 +29,6 @@ export function getEvents (callback) {
       }
     })
 }
-
-let a = {
-  'summary': 'Google I/O 2015',
-  'location': '800 Howard St., San Francisco, CA 94103',
-  'description': 'A chance to hear more about Google\'s developer products.',
-  'start': {
-    'dateTime': '2015-05-28T09:00:00-07:00',
-    'timeZone': 'America/Los_Angeles'
-  },
-  'end': {
-    'dateTime': '2015-05-28T17:00:00-07:00',
-    'timeZone': 'America/Los_Angeles'
-  },
-  'recurrence': [
-    'RRULE:FREQ=DAILY;COUNT=2'
-  ],
-  'attendees': [
-    {'email': 'lpage@example.com'},
-    {'email': 'sbrin@example.com'}
-  ],
-  'reminders': {
-    'useDefault': false,
-    'overrides': [
-      {'method': 'email', 'minutes': 24 * 60},
-      {'method': 'popup', 'minutes': 10}
-    ]
-  }
-};
 
 class Calendar extends Component {
   constructor(props) {
